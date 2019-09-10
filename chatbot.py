7a70a79861b7f8208c4020b01b46d0644fdaaec0
Himesh Siriwardana <himeshsiriwardana@Himeshs-MacBook-Air.local>
2019-09-10 13:12:20 -0400

    Imported initial libraries

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+#Building a chatbot with deep NLP
+
+#Importing the libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+########## PART 1 - DATA PROCESSING ###############3
+
